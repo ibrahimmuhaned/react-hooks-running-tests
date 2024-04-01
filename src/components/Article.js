797e9abfe47fdbf4a4c6b27e38b56846cb41588b
ibrahimmuhaned <ibrahimmuhaned999@gmail.com>
2024-04-02 01:03:55 +0300

    done

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,22 @@
+// import React from "react";
+
+// function Article() {
+//   return (
+//     <div>
+//       {/* please pass this test */}
+//     </div>
+//   );
+// }
+
+// export default Article;
+
+// Article.js
 import React from "react";
 
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+      please pass this test
     </div>
   );
 }
